6eb04a2dfa18a8bf53d74ae186e8a4a4b64c74a1
bignuttawuts <big.nuttawut.s@gmail.com>
2021-12-08 13:11:12 +0700

    initial

---
+++ src/main/java/com/bootnuttawig/authenapi/AuthenApiApplication.java
@@ -0,0 +1,13 @@
+package com.bootnuttawig.authenapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootnuttawig/authenapi/AuthenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootnuttawig.authenapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
